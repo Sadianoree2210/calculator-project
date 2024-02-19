becde43976eb70e4620fbcb16f62e3d4eebd4bac
Developers <sadianor2210@email.com>
2024-02-18 16:40:13 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+  function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+function modulus(x, y){
+    return x % y;
+}
+function calculate() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operation = document.getElementById("operation").value;
+    var result;
+
+    switch (operation) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+        case "divide":
+            result = divide(num1, num2);
+            break;
+            case "modulus":
+                result = modulus(num1, num2);
+                break;
+        default:
+            result = "Invalid operation!";
+            break;
+    }
+
+    document.getElementById('result').value = result;
+    return false;
+}
